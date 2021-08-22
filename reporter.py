dfcbfa76ae8f07500aa7d2da6b9d8b0176d919e1
FL-K88 <57526583+FL-K88@users.noreply.github.com>
2021-08-22 09:53:52 -0700

    Update reporter.py
    Now able to create basic multi-line charts for daily and monthly (30-day) time periods with simulation data.

--- reporter.py
+++ reporter.py
@@ -1,64 +1,51 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import datetime
-import array
-
+import array as arr
 
 ##Create 2 methods that receive a list of dictionaries and returns a series of graphs.
-
 ##Method 1: Daily Report
-
 ##Method 2: Rolling Monthly Report
-
 ##Why two separate methods? Is that really necessary?
-Bat1Volts = 13.45
-bilgeCycles = 2
-Temperature = 87.5
-shorePowerStatus = True
+
 logList = []
-logData = {'tempFarenheit': Temperature, 'bilgeCycles': bilgeCycles, 'shorePowerStatus': shorePowerStatus, 'Bat1Volts': Bat1Volts}
-logList.append(logData)
 
-def daily_report(dataList):
+###TEMP: Faked data for the charts. Should be provided by Jarvis.py
+for i in range(0,24):
+    Bat1Volts = 13.45
+    bilgeCycles = i
+    Temperature = 87.5
+    shorePowerStatus = True
+    logData = {'tempFarenheit': Temperature, 'bilgeCycles': bilgeCycles, 'shorePowerStatus': shorePowerStatus, 'Bat1Volts': Bat1Volts}
+    logList.append(logData)
+###END TEMP
 
-    list1 = []
-    list2 = []
-    list3 = []
-    listy1 = []
-    listy2 = []
-    listy3 = []
-    ##line chart showing voltage for each of 6 house batteries
+##Set up a line chart item with the past 24 hours of data.
+def daily_report(logList, dict_entry):
+    x_values = []
+    y_values = []
+    for i in range (0,24): 
+        x_values.append(i) ###Should this be a timestamp from the datastructure instead?
+        insert_value = logList[i]
+        y_values.append(insert_value[dict_entry])
+    plt.plot(x_values,y_values, label = dict_entry)
 
-    ##line chart showing bilge pump runs + water level?
+def monthly_report(logList, dict_entry):
+    x_values = []
+    y_values = []
+    for i in range (0, (24 * 30)): 
+        x_values.append(i) ###Should this be a timestamp from the datastructure instead?
+        insert_value = logList[i]
+        y_values.append(insert_value[dict_entry])
+    plt.plot(x_values,y_values, label = dict_entry) ##Figure out how to scale the x-values in a more practical manner
 
-    #line chart showing temperature
 
-    ##x = the count of each dict in datalist
-    ##y = the values of one variable for all the dictionaries in datalist
-    for line in range(len(dataList) - 24, len(dataList)): ##grabs the most recent hourly entries
-        list1.append(dataList[line]['Bat1Volts'])#House batteries####NEED TO INCLUDE FOR MULTIPLE BATTERIES ON SAME CHART
-        list2.append(dataList[line]['bilgeCycles'])#House batteries####NEED TO INCLUDE FOR MULTIPLE BATTERIES ON SAME CHART
-        list3.append(dataList[line]['tempFarenheit'])#House batteries####NEED TO INCLUDE FOR MULTIPLE BATTERIES ON SAME CHART
-        listy1.append(line) 
-        listy2.append(line) 
-        listy3.append(line) 
-
-    x1 = array(list1)
-    x2 = array(list2)
-    x3 = array(list3)
-    y1 = array(listy1)
-    y2 = array(listy2)
-    y3 = array(listy3)
-
-    fig, (ax1, ax2) = plt.subplots(2, sharey=False)
-    ax1.plot(x1, y1, 'ko-')
-    ax1.set(title='Daily Summary', ylabel='House Batteries (Volts)')
-    ax2.plot(x2, y2, 'r.-')
-    ax2.set(xlabel='Time of Day', ylabel='Bilge Pump Runs')
-    ##ax3.plot(x3, y3, 'r.-')
-    ##ax3.set(xlabel='Time of Day', ylabel='Temperature (F)')
-    print("report finished!")
-    plt.show()
-
+daily_report(logList, 'tempFarenheit')
+daily_report(logList, 'bilgeCycles')
+daily_report(logList, 'Bat1Volts')
+daily_report(logList, 'shorePowerStatus')
+plt.legend()
+##NOTE IF Y VALUES ARE DIFFERENT, plt.show MAY BREAK. NEED ERROR HANDLING HERE.
+plt.show()
 
 ##def monthly_report(dataList):
