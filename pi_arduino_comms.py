c47239ef60b832b74abc5ca57bdea344133f8490
= <holtnk@seas.upenn.edu>
2022-05-19 00:05:58 -0700

    logs data to list and appends timestamp.

--- pi_arduino_comms.py
+++ pi_arduino_comms.py
@@ -1,20 +1,23 @@
 ## to find current ports: portData = serial.tools.list_ports.comports()
-
+import datetime
 import serial
 from multiprocessing import Process
 
 ser = [serial.Serial('/dev/ttyACM0', baudrate = 9600, timeout = 10)] ##[Leonardo]
+logData = []
+
+##generate timestamp for data point, then store by hash of mm/yyyy?
 
 def readData(selector):
     while 1:
         arduinoData = ser[selector].readline().decode('ascii')
-        print(arduinoData)
+        ##Store data by timestamp
+        data = [datetime.datetime.now(), arduinoData]
+        logData.append(data)
+        print(logData[-1])
         
 
 if __name__ == '__main__':
     leo = Process(target = readData, args=(0,))
-    ##uno = Process(target = readData, args=(1,))
-    ##uno.start()
     leo.start()
-    ##uno.join()
     leo.join()
