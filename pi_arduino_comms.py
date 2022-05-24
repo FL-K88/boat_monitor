a15019edf9f2d61e3e990da3c248d5c18760da37
= <holtnk@seas.upenn.edu>
2022-05-24 02:22:05 -0700

    working on dailyPlot. Current issue is finding how to convert data list/array into an isostandard mm-dd-yyyy or similarly formatted value

--- pi_arduino_comms.py
+++ pi_arduino_comms.py
@@ -12,6 +12,7 @@ logData = []
 def readData(selector):
     while 1:
         arduinoData = ser[selector].readline().decode('ascii')
+        arduinoData = float(arduinoData.strip("\r\n"))
         ##Store data by timestamp
         data = [datetime.datetime.now(), arduinoData]
 
@@ -21,28 +22,29 @@ def readData(selector):
 
         logData.append(data)
         print(logData[-1])
-        time.sleep(5) ##ultimately change to 3600 (1 hour)
-
-
+        if len(logData) > 23:
+            dailyPlot(logData)
+            exit()
+        time.sleep(1) ##ultimately change to 3600 (1 hour)
 
 def dailyPlot(logData):
-    plt.style.use('_mpl-gallery')
     sl = slice(-24,-1) 
     data = np.array(logData[sl])
     np.append(data,logData[-1]) ##accounts for slice not including final element
-    print(data[0][0])
-    print(data[-1][0])
+    #print(data[0][0])
+    #print(data[-1][0])
     maxDate = data[0][0] ##assumes logData is ordered with first element earliest
     minDate = data[-1][0]
-    x = data[:,0]
+    ##print(minDate.strftime('%Y-%m-%d'))
+    ##print(maxDate.strftime('%Y-%m-%d'))
+    x = data[:,0].astype(datetime.datetime.strftime('%Y-%m-%d'))
     y = data[:,1]
-    print(x,y)
-    fig,ax = plt.subplots()
-    ax.plot(x,y,linewidth=2.0)
-    ax.set(xlim=(0, 8), xticks=np.arange(1, 8),
-       ylim=(0, 8), yticks=np.arange(1, 8))
-    plt.show()
-        
+    print("x:", x)
+    print("y:", y)
+    plt.plot(x,y,linewidth=2.0)
+    ##plt.set(xlim=(0, 8), xticks=np.arange(1, 8),
+    ##   ylim=(0, 8), yticks=np.arange(1, 8))
+    plt.show() 
 
 if __name__ == '__main__':
     leo = Process(target = readData, args=(0,))
