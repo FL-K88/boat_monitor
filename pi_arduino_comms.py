ac81d7a861c39deac37f6a3b9d3b30b7c164d8d5
= <holtnk@seas.upenn.edu>
2022-07-06 03:44:06 -0700

    Still not printing chart. Added comments for sharing on GH.

--- pi_arduino_comms.py
+++ pi_arduino_comms.py
@@ -7,15 +7,19 @@ import matplotlib
 from matplotlib import pyplot as plt
 import numpy as np
 
-ser = [serial.Serial('/dev/ttyACM0', baudrate = 9600, timeout = 10)] ##[Leonardo]
+
+ser = [serial.Serial('/dev/ttyACM0', baudrate = 9600, timeout = 10)] ##[Arduino Leonardo]
 logData = []
 def readData(selector):
     while 1:
+        ##arduino transmits 1 voltage reading per second to the pi.
         arduinoData = ser[selector].readline().decode('ascii')
         arduinoData = float(arduinoData.strip("\r\n"))
-        ##Store data by timestamp
+
         if isinstance(arduinoData,float) == False:
             print("voltage stored as " + str(type(arduinoData)) + ", not float")
+        
+        ##Store data by timestamp
         data = [datetime.datetime.now(), arduinoData]
 
         ##drop the earliest element if more than a year's worth of data is stored
@@ -27,7 +31,7 @@ def readData(selector):
         if len(logData) > 23:
             dailyPlot(logData)
             exit()
-        time.sleep(1) ##ultimately change to 3600 (1 hour)
+        time.sleep(1) ##ultimately change to 3600/1 hour
 
 def dailyPlot(logData):
     sl = slice(-24,-1) 
@@ -35,26 +39,25 @@ def dailyPlot(logData):
     data.append(logData[-1]) ##accounts for slice not including final element
     maxDate = data[0][0] ##assumes logData is ordered with first element earliest
     minDate = data[-1][0]
+
     ## print(minDate.strftime('%Y-%m-%d'))
     ## print(maxDate.strftime('%Y-%m-%d'))
-    x = np.arange(start=0,stop=24,step=1) ## hours offset. Temporary
+    x = np.arange(start=0,stop=24,step=1) ## time offset. Temporary. Insures chart is getting only ints and floats
     y = np.asarray(data)[:,1]
     if not isinstance(x, list):
         x = x.tolist()
     if not isinstance(y, list):
         y = y.tolist()
-    print("x:", x)
-    print("y:", y)
+    ##print("x:", x)
+    ##print("y:", y)
+
     plot = plt.figure()
     plt.plot(x, y)
-    ##ax.step(x,y,linewidth=2.0)
-    ##ax.set(xlim=(0,24),xticks=np.arange(1,24), ylim=(0,20), yticks=(np.arange(1,20)))
-    ##plt.set(xlim=(0, 8), xticks=np.arange(1, 8),
-       ##ylim=(0, 8), yticks=np.arange(1, 8))
-
-    plt.show(block = True)
+    plt.show()
 
 if __name__ == '__main__':
+    ##multithreading for lolz. 
+    ##Ultimately going to try separate processes for each battery class, water level monitor
     p = Process(target = readData, args=(0,))
     p.start()
     p.join()
